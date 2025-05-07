ce3dbd0b7fa35397e11aedc82437cb8d12f41c87
Loai Hataba <LoaiHataba@gmail.com>
2025-05-07 20:17:56 +0300

    nevermind

--- section.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    cout << "Loai Hataba id: 20230553\n";
-    cout << "Hossam Abdelaziz id: 20230121\n";
-    cout << "Abdullah Mohamed id: 20230231\n";
-}
