6cd50c7b197745bed401bad9ab82f3b22961379b
Loai Hataba <LoaiHataba@gmail.com>
2025-05-09 23:39:17 +0300

    sample use of ui import in main

--- Assignment #2/App/budgetapp/src/main/java/com/budgetapp/Main.java
+++ Assignment #2/App/budgetapp/src/main/java/com/budgetapp/Main.java
@@ -1,7 +1,11 @@
 package com.budgetapp;
 
+import com.budgetapp.system.UI;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        UI myUI = new UI();
+        
     }
 }
---
+++ Assignment #2/App/budgetapp/src/main/java/com/budgetapp/system/UI.java
@@ -0,0 +1,7 @@
+package com.budgetapp.system;
+
+public class UI {
+    public UI() {
+        System.out.println("UI object has been created!");
+    }
+}
