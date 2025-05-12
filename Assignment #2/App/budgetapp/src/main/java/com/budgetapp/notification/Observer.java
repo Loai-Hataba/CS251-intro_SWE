9bd15c4fae4ed0b4ab440ad8dec34bd1fc630293
Abdelaziz28 <162904583+HosStdeez@users.noreply.github.com>
2025-05-13 00:24:39 +0300

    observer typeshi

--- Assignment #2/App/budgetapp/src/main/java/com/budgetapp/notification/Observer.java
+++ Assignment #2/App/budgetapp/src/main/java/com/budgetapp/notification/Observer.java
@@ -1,10 +1,8 @@
 package com.budgetapp.notification;
 
-// import com.budgetapp.reminder.Subject;
-
 public abstract class Observer {
-    // Subject subject;
-    void update(String event) {
+
+    public void update(String event) {
         // Default implementation (can be overridden)
     }
 }
