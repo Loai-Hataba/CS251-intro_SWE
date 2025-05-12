78050e114fc62aa181ecc132a5b636a4f5748a9d
Abdelaziz28 <162904583+HosStdeez@users.noreply.github.com>
2025-05-12 22:14:45 +0300

    setting notification manager

---
+++ Assignment #2/App/budgetapp/src/main/java/com/budgetapp/notification/NotificationManager.java
@@ -0,0 +1,40 @@
+package com.budgetapp.notification;
+
+import com.budgetapp.database.Records;
+import com.budgetapp.methods.Methods;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationManager {
+
+    // Singleton instance
+    private static NotificationManager instance;
+
+    // Message to be used in notification
+    private String msg;
+
+    // Private constructor to prevent instantiation
+    private NotificationManager() {
+    }
+
+    // Singleton accessor
+    public static NotificationManager getInstance() {
+        if (instance == null) {
+            instance = new NotificationManager();
+        }
+        return instance;
+    }
+
+    // Creates a notification with the given message
+    public boolean createNotification(String message) {
+        this.msg = message;
+        System.out.println("Notification created: " + msg);
+        return true;
+    }
+
+    // Updates the notification based on an event
+    public void update(String event) {
+        System.out.println("Notification updated due to event: " + event);
+        // Add update logic here
+    }
+}
